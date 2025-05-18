213e1df81a4742cd354145525cdde526ef1a2675
nikolozgamtenazde2011 <gamtenazdenikolozi@gmail.com>
2025-05-18 12:52:26 +0400

    commit

---
+++ day 04/homework/homework.py
@@ -0,0 +1,46 @@
+#დავალება=შექმენი 20-30 error(არასწორი კოდი) და გვერზდე მიუწერე სწორი კოდის პორმა 
+
+object->"21'  "
+         #object=(21)
+print(object"
+      #print("object")
+"songname=stronger'
+       #songname=stronger
+number4='27'
+    # number=27
+b"ones=(200+6"
+        #bones=(200+6)
+level=1*4
+        #level=(1*4)
+number2={99/11}    
+        #number2=(99/11) 
+print(door)
+     #print("door")
+pilows(3)(
+        #pilows(3)
+time=9:30
+        #time=(9:30)
+   #questions(7)
+        #questions(7)   (კოდს როცა იწყებ არ უნდა დაიწყოს ცრიელი სივრცით)
+walls==(4)
+        #walls=(4)  
+hats(30/5)""
+        #hats=(30/5)
+bottles22-17()
+       #bottles(22-17)
+
+
+
+
+        
+
+  
+
+
+ 
+
+       
+
+
+      
+      
