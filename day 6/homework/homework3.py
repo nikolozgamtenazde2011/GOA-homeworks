cac2ddddb360f54a9de5091da2286c481f5d31d5
nikolozgamtenazde2011 <gamtenazdenikolozi@gmail.com>
2025-06-19 00:09:25 +0400

    commit

--- day 6/homework/homework3.py
+++ day 6/homework/homework3.py
@@ -1,6 +1,7 @@
-number1=(23)
-number2=(46)
-number3=(number1+number2)
-number4=(number2/number1)
-number5=(55)
-average=int(number1,number2,number3,number4,number5)
+number1=int(input("enter first number:"))
+number2=int(input("enter second number:"))
+number3=int(input("enter third number:"))
+number4=int(input("enter fourth number:"))
+number5=int(input("enter fifth number:"))
+average =(number1,number2,number3,number4,number5) / 5
+print("The average of the five numbers is:", average)
