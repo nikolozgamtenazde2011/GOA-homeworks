0281443a0770443b84d7c993cb05e57608f1661e
nikolozgamtenazde2011 <gamtenazdenikolozi@gmail.com>
2025-06-22 19:35:33 +0400

    commit

---
+++ day 08/classwork/classwork.py
@@ -0,0 +1,3 @@
+age=input("Enter your age: ")
+age2=input("Enter your age: ")
+both=int("age")+int("age2")
---
+++ day 08/homework/homework2.py
@@ -0,0 +1,11 @@
+name = 'nikoloz'
+surname = "gamtenazde"
+age = 13
+weight = 67
+height = 157
+
+print(type(name))
+print(type(surname))
+print(type(age))
+print(type(weight))
+print(type(height))
